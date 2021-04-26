8d29b864448514100201d1d61c62d566a6f3a061
ardasalisha <ardasalisha@gmail.com>
2021-04-26 23:06:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recycleviewcatatankeuangan/MainActivity.java
@@ -0,0 +1,31 @@
+
+package com.example.recycleviewcatatankeuangan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    String s1[], s2[];
+    int image[] = {R.drawable.background, R.drawable.profile,R.drawable.background,R.drawable.profile,
+            R.drawable.background, R.drawable.profile, R.drawable.background, R.drawable.profile};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        s1 = getResources().getStringArray(R.array.list_record);
+        s2 = getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1,s2,image);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+}
